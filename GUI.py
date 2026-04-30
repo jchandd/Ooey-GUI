109f4689dde808386b987b9dbf72fc4a1f155f72
jchandd <jaydenc243@deltalearns.ca>
2026-04-30 12:36:14 -0700

    added in more functions will work on main window soon

--- GUI.py
+++ GUI.py
@@ -1,11 +1,22 @@
-#TIP CALCULATOR
-#Able to calculate tip from total bill
-#Calculates and adds of tip and total bill
+# TIP CALCULATOR
+# Able to calculate tip from total bill
+# Calculates and adds of tip and total bill
 
 import tkinter as tk
 
-#Calculate tip and total function
-def calculate():
-    bill = float(entry.get()) #Get the total bill from the entry field
 
-    tip_percent = slider.get() #Get the tip percentage from the slider
+# Calculate tip and total function
+def calculate():
+    bill = float(entry.get())  # Get the total bill from the entry field
+
+    tip_percent = slider.get()  # Get the tip percentage from the slider
+
+    tip_amount = bill * (tip_percent / 100)  # Calculate the tip amount
+
+    total_amount = bill + tip_amount  # Calculate the total amount
+    tip_label.config(
+        text=f"Tip Amount: ${tip_amount:.2f}"
+    )  # Update the tip amount label
+    total_label.config(
+        text=f"Total Amount: ${total_amount:.2f}"
+    )  # Update the total amount label
